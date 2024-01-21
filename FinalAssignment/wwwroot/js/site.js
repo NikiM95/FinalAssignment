b38d1a62ae7571fbc9906ac001542bfebb3af507
NikiM95 <nicolemongan95@gmail.com>
2024-01-21 08:35:07 +0000

    Add project files.

---
+++ FinalAssignment/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+//Function to be called onmouseover and onmouseout to change the color of "My favourite song!"
+function newColor() {
+    document.getElementById("final_assignment").style.color = "white";
+}
+
+function oldColor() {
+    document.getElementById("final_assignment").style.color = "black";
+}
